fff71ad72e0acf81ceadfa53aca3f02a8850703f
Alexandr Korotkov <splean.1@yandex.ru>
2017-06-26 22:26:06 +0300

    add crawler.py

---
+++ crawler.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def get_shop_by_links(html):
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    headings = soup.find('aside', id="secondary").find_all('h5')
+    #print(headings)
+    links_headings = []
+
+    for heading in headings:
+        a = heading.find('a')#.get('href')
+        #print(a)
+        if a is None:
+            continue
+        else:
+            a = a.get('href')
+            #print(a)
+        link = 'https://www.perriconemd.com' + a
+        #print(link)
+        links_headings.append(link)
+    
+    print(links_headings)
+    
+    return links_headings
+
+
+
+def main():
+    url = 'https://www.perriconemd.com/skincare/shop-by-product/'
+    links = get_shop_by_links( get_html(url) )
+
+# На выходе получает 3 ссылки:
+# Основные товары
+# https://www.perriconemd.com/skincare/shop-by-product/
+
+# Наборы
+# https://www.perriconemd.com/skincare-sets/
+
+# Добавки
+# https://www.perriconemd.com/supplements-shop-all/
+
+
+if __name__ == '__main__':
+    main()
